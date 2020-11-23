a1da6a88ccbdc155770aaa95e70919547bb60d61
MINWOO <augustman8808@gmail.com>
2020-11-23 23:53:41 +0900

    files added

---
+++ spring_batch_partitioning/src/main/java/com/batch/demo/business/batch/mapper/studyMapper.java
@@ -0,0 +1,7 @@
+package com.batch.demo.business.batch.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class studyMapper {
+}
---
+++ spring_batch_partitioning/src/main/java/com/batch/demo/framework/context/config/DataSourceConfiguration.java
@@ -0,0 +1,74 @@
+package com.batch.demo.framework.context.config;
+
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableConfigurationProperties
+@EnableTransactionManagement
+@MapperScan(basePackages = "com.batch.demo.**.mapper", sqlSessionFactoryRef = "sqlSessionFactory")
+public class DataSourceConfiguration {
+
+    /**
+     * dataSource
+     *
+     * @return
+     */
+    @Primary
+    @Bean(name="dataSource")
+    @ConfigurationProperties(prefix="spring.datasource.hikari")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    /**
+     * sqlSessionFactory
+     * @param dataSource
+     * @param applicationContext
+     *
+     * @return
+     * @throws Exception
+     */
+    @Bean(name="sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource, ApplicationContext applicationContext) throws java.lang.Exception{
+        System.out.println("dataSource : "+dataSource);
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setTypeAliasesPackage("com.nws.zabbix.api.busineess.batvch.vo");
+        sqlSessionFactoryBean.setConfigLocation(applicationContext.getResource("classpath:mybatis-config.xml"));
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:mapper/**/*.xml"));
+        sqlSessionFactoryBean.setVfs(SpringBootVFS.class);
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    /**
+     * transactionManager
+     * @param dataSource
+     *
+     * @return
+     * @throws Exception
+     */
+    @Bean(name="transactionManager")
+    public PlatformTransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource) throws java.lang.Exception {
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager(dataSource);
+        dataSourceTransactionManager.setGlobalRollbackOnParticipationFailure(false);
+        return dataSourceTransactionManager;
+    }
+
+}
